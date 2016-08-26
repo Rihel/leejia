1703fd45286228100b1430f6e30a8705136476e1
Rihel <569843207@qq.com>
2016-08-26 16:10:31 +0800

    样式修改
    样式修改

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,17 @@ $(function(){
 		direction:"horizontal",
 		easing:"swing",
 		slideshowSpeed: 5500
-	})
+	});
+	active($('.nav'),'li');
+	active($('.section-2-content'),'li');
+	active($('.sign-box'),'a');
+	active($('.users'),'li');
+	active($('.section-5-list'),'li')
 })
+
+function active(ele,child){
+	ele.find(child).hover(function(){
+		ele.find(child).removeClass('active')
+		$(this).addClass('active');
+	})
+}
