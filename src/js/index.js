7a2efff61e342c9c1e210e0bffbc4ee137f8f1b3
Rihel <569843207@qq.com>
2016-08-26 18:07:18 +0800

    更新
    跟新

--- src/js/index.js
+++ src/js/index.js
@@ -9,12 +9,20 @@ $(function(){
 	active($('.section-2-content'),'li');
 	active($('.sign-box'),'a');
 	active($('.users'),'li');
-	active($('.section-5-list'),'li')
+	active($('.section-5-list'),'li');
+
+
+	$('.section-2-tab').find('span').click(function(){
+		$('.section-2-tab').find('span').removeClass('active');
+		$(this).addClass('active');
+		$('.section-2').find('.section-2-content').stop().fadeOut();
+		$('.section-2').find('.section-2-content').eq($(this).index()).stop().fadeIn();
+	})
 })
 
 function active(ele,child){
 	ele.find(child).hover(function(){
 		ele.find(child).removeClass('active')
 		$(this).addClass('active');
-	})
+	});
 }
